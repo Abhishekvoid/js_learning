52d354dcc69c85eff5fbf41e75f63121a84cc25c
Abhishekvoid <91648941+Abhishekvoid@users.noreply.github.com>
2023-03-29 15:11:59 +0530

    a temperature converter

---
+++ day-1/Kelvin_Weather.js
@@ -0,0 +1,20 @@
+// declare a variable name kelvin and assign value 293 
+const kelvin = 293;
+console.log(`The temperature is ${kelvin} degrees celsius`)
+//  declare a variable name celsius and assign value by sub kelvin with 273
+let celsius;
+celsius = kelvin - 273;
+console.log(`The temperature is ${celsius} degrees celsius`)
+
+//  declare a variable name fahrenheit and assign value by following function fahrenheit = celsius*(9/5)+32; and round it's value
+let fahrenheit;
+fahrenheit = celsius*(9/5)+32;
+fahrenheit = Math.floor(fahrenheit);
+
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`);
+
+let newton;
+newton = celsius*(33/100);
+newton = Math.floor(newton);
+
+console.log(`The temperature is ${newton} degrees newton.`);
